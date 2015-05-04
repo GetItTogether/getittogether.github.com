3db88143537fb6bb8a325a3949bb40b14ebb85d6
Mike Stewart <stewart@MediaDoneRight.com>
2015-05-04 10:51:45 -0700

    feat: change point where navbar appears

--- tools/js/rockon.js
+++ tools/js/rockon.js
@@ -16,7 +16,7 @@ function resizeDiv() {
 };
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 100) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
